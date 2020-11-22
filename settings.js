0a1e92301b82003b624d6fd57942c92c8bc93536
Nutthapat <nutp1999@gmail.com>
2020-11-22 16:24:43 +0700

    initial commit

--- settings.js
+++ settings.js
@@ -33,6 +33,15 @@ var settings = module.exports = {
     // post-deploy are restored after a restage
     autoInstallModules: true,
 
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$5A68hAFDYixPgZina37tiOrrtgd5p01krXi5DxYqnENc5KJpajyDG",
+            permissions: "*"
+        }]
+     },
+
     // Move the admin UI
     httpAdminRoot: '/editor',
     
@@ -66,23 +75,23 @@ var settings = module.exports = {
     }
 }
 
-if (process.env.NODE_RED_a_USERNAME && process.env.NODE_RED_b_PASSWORD) {
-    settings.adminAuth = {
-        type: "credentials",
-        users: function(username) {
-            if (process.env.NODE_RED_a_USERNAME == username) {
-                return when.resolve({username:username,permissions:"*"});
-            } else {
-                return when.resolve(null);
-            }
-        },
-        authenticate: function(username, password) {
-            if (process.env.NODE_RED_a_USERNAME == username &&
-                process.env.NODE_RED_b_PASSWORD == password) {
-                return when.resolve({username:username,permissions:"*"});
-            } else {
-                return when.resolve(null);
-            }
-        }
-    }
-}
+// if (process.env.NODE_RED_a_USERNAME && process.env.NODE_RED_b_PASSWORD) {
+//     settings.adminAuth = {
+//         type: "credentials",
+//         users: function(username) {
+//             if (process.env.NODE_RED_a_USERNAME == username) {
+//                 return when.resolve({username:username,permissions:"*"});
+//             } else {
+//                 return when.resolve(null);
+//             }
+//         },
+//         authenticate: function(username, password) {
+//             if (process.env.NODE_RED_a_USERNAME == username &&
+//                 process.env.NODE_RED_b_PASSWORD == password) {
+//                 return when.resolve({username:username,permissions:"*"});
+//             } else {
+//                 return when.resolve(null);
+//             }
+//         }
+//     }
+// }
